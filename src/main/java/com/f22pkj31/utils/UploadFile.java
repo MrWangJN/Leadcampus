9d183ee7ffedb414b9673eea4a0f70193678bf5d
F22PKJ31 <1136286586@qq.com>
2018-05-29 10:06:34 +0800

    log

--- src/main/java/com/f22pkj31/utils/UploadFile.java
+++ src/main/java/com/f22pkj31/utils/UploadFile.java
@@ -2,8 +2,8 @@ package com.f22pkj31.utils;
 
 public class UploadFile {
 	public final static String STUPICURL = "img/studentpic/";
-	public final static String URL = "C:/2_workspace/6_Files/leadcompusfile/";
-//	public final static String URL = "/home/tomcat/apache-tomcat-8.5.8/webapps/leadcompusfile/";
-	public final static String SERVICEUTL = "http://47.95.14.18:8080/leadcampus/";
+//	public final static String URL = "C:/2_workspace/6_Files/leadcompusfile/";
+	public final static String URL = "/com/f22pkj31/file/leadcompusfile/";
+	public final static String SERVICEUTL = "http://59.110.42.194:8080/leadcampus/";
 
 }
